1a60a52a18528bd4060735938f479cd5026ac89c
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-05-14 16:05:03 +0300

    oof

--- src/testcommand.js
+++ src/testcommand.js
@@ -2,7 +2,7 @@ module.exports = {
     execute: async function(message, args, util) {
         let config = util.apis["ccm-ccm"].config["ccm"];
         let cls = util.apis["core-cls"].api;
-        if(!config.owners.includes(message.author.id)) return message.reply(cls.getString("core", "error.permission"));
+        if(!util.config.owners.includes(message.author.id)) return message.reply(cls.getString("core", "error.permission"));
         if(!args[0] || !args[1]) return message.channel.send(cls.getString("ccm", "usage"))
         if (args[0] == "get") {
             message.channel.send(config?.[args[1]])
