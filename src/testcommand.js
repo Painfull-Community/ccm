5127e99213392b83fb4bb1b5df0244522949809f
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-05-14 16:09:20 +0300

    Update testcommand.js

--- src/testcommand.js
+++ src/testcommand.js
@@ -3,12 +3,13 @@ module.exports = {
         let config = util.apis["ccm-ccm"].config["ccm"];
         let cls = util.apis["core-cls"].api;
         if(!util.config.owners.includes(message.author.id)) return message.reply(cls.getString("core", "error.permission"));
-        if(!args[0] || !args[1]) return message.channel.send(cls.getString("ccm", "usage"))
+        if(!args[0]) return message.channel.send(cls.getString("ccm", "usage"))
         if (args[0] == "get") {
+            if(!args[1]) return message.channel.send(cls.getString("ccm", "usage"))
             message.channel.send(config?.[args[1]])
         }
         if (args[0] == "set") {
-            if(!args[2]) return message.channel.send(cls.getString("ccm", "usage"))
+            if(!args[1] || !args[2]) return message.channel.send(cls.getString("ccm", "usage"))
             config[args[1]] = args[2]
             util.apis["ccm-ccm"].api.save("ccm")
         }
